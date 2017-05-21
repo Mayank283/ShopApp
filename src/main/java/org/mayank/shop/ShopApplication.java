ad199842baa5fcbdb06108be701b553eff27dd1a
Mayank <mayank283@live.com>
2017-05-21 13:29:23 +0530

    Setting up the Project

---
+++ src/main/java/org/mayank/shop/ShopApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.mayank.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mayank
+ *
+ */
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+
+	}
+}
